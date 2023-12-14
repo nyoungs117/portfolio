2a879b7ad68b07f703b6eae453998568b505132d
Oh <nyoungs117@gmail.com>
2023-12-15 01:47:39 +0900

    완성

--- js/index.js
+++ js/index.js
@@ -58,60 +58,235 @@ $('a[href*="#"]').on('click', function (e) {
 
 
 
+// 처음로딩이 되거나 새로고침 또는 화면에 섹션이 도달할때마다 
+// 2초후에 계속 모션이 실행되도록 
 
 $(document).ready(function() {
+  $('.skill1, .skill2, .skill3, .skill4, .skill5, .skill6, .skill7').draggable();
+  $('.skill1, .skill2, .skill3, .skill4, .skill5, .skill6, .skill7').click(function() {
+    $('.skill1, .skill2, .skill3, .skill4, .skill5, .skill6, .skill7').css('z-index', 1);
+    $(this).css('z-index', 2);
+  });
 
-    $('.skill1, .skill2, .skill3, .skill4, .skill5, .skill6, .skill7').draggable(); // 각 skill 클래스를 가진 요소를 드래그 가능하도록 만듦
-
-    $('.skill1, .skill2, .skill3, .skill4, .skill5, .skill6, .skill7').click(function() {
-        // 모든 .skill 요소들의 z-index를 1로 설정
-        $('.skill1, .skill2, .skill3, .skill4, .skill5, .skill6, .skill7').css('z-index', 1);
-    
-        // 클릭한 요소의 z-index를 더 높은 값으로 변경하여 최상위로 올림
-        $(this).css('z-index', 2);
-      });
-    
+  function showSkills() {
     var skills = ['.skill3', '.skill6', '.skill5', '.skill2', '.skill4', '.skill1', '.skill7'];
-    $('.skill > div').css('opacity', 0); // 페이지 로드 시 모든 스킬을 투명하게 만듦
+    $('.skill > div').css('opacity', 0).stop(true, true); // stop any ongoing animation
 
     function showNextSkillWithDelay() {
       if (skills.length > 0) {
-        var skillToShow = skills.shift(); // 배열에서 다음 스킬 가져오기
-        var delayTime = 50; // 기본 딜레이
+        var skillToShow = skills.shift();
+        var delayTime = 50;
         if (skillToShow === '.skill2') {
-          delayTime = 800; // .skill5에서 .skill2로 넘어갈 때 3초의 딜레이 설정
+          delayTime = 800;
         }
         $(skillToShow).delay(delayTime).animate({ opacity: 1 }, 50, function() {
-          showNextSkillWithDelay(); // 다음 스킬 나타내기
+          showNextSkillWithDelay();
         });
       }
     }
-
+    
     setTimeout(function() {
-      showNextSkillWithDelay(); // 2초 뒤에 순차적인 스킬 보이기 시작
-    }, 2100); // 2초 뒤에 시작되도록 설정
+      showNextSkillWithDelay();
+    }, 2100);
+  }
+
+  // 페이지 로드 시에 스킬 모션 시작
+  showSkills();
+
+  // 스크롤 이벤트를 감지하여 해당 섹션에 도달하면 스킬들이 나타나도록 함
+  $(window).scroll(function() {
+    var skillPosition = $('.skill').offset().top;
+    var scrollPosition = $(window).scrollTop();
+    if (scrollPosition + $(window).height() / 2 > skillPosition) {
+      setTimeout(function() {
+        showSkills();
+      });  // 스크롤이 아래서 위로 올라왔을 때도 2초 후에 스킬 모션이 시작됩니다.
+    }
+  });
+});
+
+
+
+//스킬버튼 깜빡거리도록
+
+// 요소 선택 후 클래스 토글링
+$(document).ready(function() {
+  setInterval(function() {
+      $('.s4').toggleClass('active');
+  }, 500); // 1초 간격으로 클래스 토글
+
+});
+
+
+
+
+
+
+
+
+  // #aboutme 에 화면이 도달하면 svg도 그려지고 
+  // strong으로 된 텍스트가 1초 후 순차적으로 컬러가 바뀌게
+
+  var isDrawn = false;
+
+  function drawSVG() {
+    var svg = document.querySelector('svg');
+    var path = document.querySelector('.path');
+    var length = path.getTotalLength();
+    path.style.transition = path.style.WebkitTransition = 'none';
+    path.style.strokeDasharray = length + ' ' + length;
+    path.style.strokeDashoffset = length;
+    path.getBoundingClientRect();
+    path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 2s ease-in-out';
+    path.style.strokeDashoffset = '0';
+    svg.style.opacity = '0.3';
+    
+    // 색상 변경 코드
+    setTimeout(() => {
+      var strongTags = document.querySelectorAll('#aboutme ul li strong');
+      strongTags.forEach((tag, index) => {
+        setTimeout(() => {
+          tag.style.color = '#DDFF21';
+          tag.style.transition = 'color 0.5s ease-in-out';  // 색상 변경에 애니메이션 효과를 추가합니다.
+        }, index * 500);  // 500ms 간격으로 각각의 태그에 대해 색상 변경을 실행합니다.
+      });
+    }, 1000);  // 색상 변경을 1초 후에 시작합니다.
+  }
+  
+  function resetSVG() {
+    var svg = document.querySelector('svg');
+    var path = document.querySelector('.path');
+    svg.style.opacity = '0';
+    path.style.strokeDashoffset = path.getTotalLength();
+    
+    // 색상 변경 코드
+    var strongTags = document.querySelectorAll('#aboutme ul li strong');
+    strongTags.forEach(tag => {
+      tag.style.color = '';
+      tag.style.transition = 'color 0.5s ease-in-out';  // 색상 변경에 애니메이션 효과를 추가합니다.
+    });
+  }
+  
+  window.addEventListener('scroll', function() {
+    var aboutMePosition = document.getElementById('aboutme').getBoundingClientRect().top;
+  
+    if(aboutMePosition < window.innerHeight / 2 && aboutMePosition > 0) {
+      if(!isDrawn) {
+        drawSVG();
+        isDrawn = true;
+      }
+    } else {
+      if(isDrawn) {
+        resetSVG();
+        isDrawn = false;
+      }
+    }
   });
 
 
 
 
 
+ 
 
-
-
-//스크롤 따른 svg 변화
+// work 섹션에 화면이 도달하면 ul li들이  순차적으로 나타나도록
 
 $(document).ready(function() {
-  var path = $('.path')[0];
-  var pathLength = path.getTotalLength();
+  var workOffset = $('#work').offset().top; // work 섹션의 위치를 계산
 
+  // 스크롤 이벤트를 감지
   $(window).scroll(function() {
-      var blockBounds = $('#image')[0].getBoundingClientRect();
-      var screenHeight = $(window).height();
-      var scroll1percent = (screenHeight - blockBounds.y - 300) / (blockBounds.height + screenHeight - 10) * 250;
+      var scrollTop = $(window).scrollTop(); // 현재 스크롤 위치를 계산
 
-      var currentLength = (pathLength * scroll1percent) / 100;
-      $(path).css("stroke-dasharray", currentLength + " " + pathLength);
+      // 현재 스크롤 위치가 work 섹션의 위치에 도달하면
+      if (scrollTop >= workOffset) {
+          // 각 요소의 opacity를 1초 간격으로 1로 변경
+          $('.wk1').delay(500).animate({opacity: 1});
+          $('.wk2').delay(1500).animate({opacity: 1});
+          $('.wk3').delay(2500).animate({opacity: 1});
+      }
+  });
+});
+
+
+
+
+
+
+//work 섹션중 shakeat 모달창
+
+$(document).ready(function() {
+  // 이미지를 클릭했을 때, 모달창이 부드럽게 나타나게 합니다.
+  $('.w_k .sh_img').on('click', function() {
+      $('#wk1_modal').fadeIn('slow');
+  });
+
+  $('.w_k .b_img').on('click', function() {
+    $('#wk2_modal').fadeIn('slow');
+});
+
+$('.w_k .gs_img').on('click', function() {
+  $('#wk3_modal').fadeIn('slow');
+});
+
+
+  // 모달창 내부의 버튼을 클릭했을 때, 모달창이 부드럽게 사라지게 합니다.
+  $('#wk1_modal').on('click', 'button', function() {
+      $('#wk1_modal').fadeOut('slow');
+  });
+
+  $('#wk2_modal').on('click', 'button', function() {
+    $('#wk2_modal').fadeOut('slow');
+});
+
+$('#wk3_modal').on('click', 'button', function() {
+  $('#wk3_modal').fadeOut('slow');
+});
+
+
+});
+
+
+
+
+
+
+
+//자기소개 애니메이션 
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    var contactPosition = $('#contact').offset().top;
+    var scrollPosition = $(window).scrollTop();
+    var windowHeight = $(window).height();
+
+    if (scrollPosition + windowHeight > contactPosition) {
+      const text = $('.ct_contentsleft p').html().split('<br>'); // 텍스트를 <br> 태그를 기준으로 분할
+      $('.ct_contentsleft p').empty(); // 텍스트 내용 초기화
+
+      text.forEach(function(line, index) {
+        if (index !== 0) {
+          $('.ct_contentsleft p').append('<br>'); // <br> 태그 추가
+        }
+
+        for (let i = 0; i < line.length; i++) {
+          const span = $('<span>')
+            .text(line[i])
+            .css({
+              color: 'gray',
+              transition: 'color 0.5s ease-in-out',
+            })
+            .appendTo('.ct_contentsleft p');
+
+          setTimeout(function() {
+            span.css('color', '#ffffff');
+          }, 70 * (index * text[0].length + i));
+        }
+      });
+
+      // 한 번만 실행되도록 이벤트 제거
+      $(window).off('scroll');
+    }
   });
 });
 
@@ -121,26 +296,26 @@ $(document).ready(function() {
 
 
 
-//section importantskill 부분 스크롤시 왼쪽으로 천천히 이동
-const scrollableList = document.querySelector('.scrollable-list');
-const originalListItems = document.querySelectorAll('.scrollable-list li');
-const numOfItemsToClone = 14;
 
-for (let i = 0; i < numOfItemsToClone; i++) {
-    originalListItems.forEach((item) => {
-        const clone = item.cloneNode(true);
-        scrollableList.appendChild(clone);
-    });
-}
 
-scrollableList.addEventListener('mouseenter', function() {
-    scrollableList.classList.add('paused');
+
+
+// 내용복사
+
+$(document).ready(function() {
+  $('#contact ul li').on('click', function() {
+    let textToCopy = $(this).data('text');
+    
+    let tempTextarea = $('<textarea>').val(textToCopy).css({
+      position: 'absolute',
+      left: '-9999px'
+    }).appendTo('body').select();
+    
+    document.execCommand('copy');
+    
+    tempTextarea.remove();
+
+    // 복사가 완료된 후 알림창을 띄웁니다.
+    alert('복사되었습니다: ' + textToCopy);
+  });
 });
-
-scrollableList.addEventListener('mouseleave', function() {
-    scrollableList.classList.remove('paused');
-});
-
-
-
-
--- js/mainskillpopup.js
+++ js/mainskillpopup.js
@@ -23,16 +23,136 @@ $(document).ready(function() {
     let CaltWidth = "23vw";
     let CaltHeight = "12.5vw";
     let CaltFontSize = "1.5vw";
-    let CaltFontColor = "#F9613F";
+    let CaltFontColor = "#DDFF21";
     let CMarginRight = "0.3vw";
     let CMarginLeft = "5vw";
   
     let currentState = 'original'; // 초기 상태
   
-    $(".s").click(function() {
+    //es5
+    $(".s1").click(function() {
+        if (currentState !== 'salt') {
+            $(".skill1").css({"width": SaltWidth, "height": SaltHeight});
+            $(".ms1b h1").text("표준화된 자바스크립트 언어를 배우고 있습니다.");
+            $(".ms1b h1").css({"font-size": SaltFontSize, "color": originalFontColor});
+            $(".ms1t ul").css("margin-left", SMarginLeft);
+            $(".ms1t ul li").css("margin-right", SMarginRight);
+            currentState = 'salt';
+        } else {
+            $(".skill1").css({"width": originalWidth, "height": originalHeight});
+            $(".ms1b h1").text("ES5");
+            $(".ms1b h1").css("font-size", originalFontSize);
+            $(".ms1b h1").css("color", originalFontColor);
+            $(".ms1t ul").css("margin-left", originalMarginLeft);
+            $(".ms1t ul li").css("margin-right", originalMarginRight);
+            currentState = 'original';
+        }
+    });
+  
+    $(".b1").click(function() {
+        if (currentState !== 'balt') {
+            $(".skill1").css({"width": BaltWidth, "height": BaltHeight});
+            $(".ms1b h1").text("더 나아가 ES6까지 할 수 있으며, javascript를 효율적이고 기본을 지키며 작성할 수 있도록 할 예정니다.");
+            $(".ms1b h1").css({"font-size": BaltFontSize, "color": originalFontColor});
+            $(".ms1t ul").css("margin-left", BMarginLeft);
+            $(".ms1t ul li").css("margin-right", BMarginRight);
+            currentState = 'balt';
+        } else {
+            $(".skill1").css({"width": originalWidth, "height": originalHeight});
+            $(".ms1b h1").text("ES5");
+            $(".ms1b h1").css("font-size", originalFontSize);
+            $(".ms1b h1").css("color", originalFontColor);
+            $(".ms1t ul").css("margin-left", originalMarginLeft);
+            $(".ms1t ul li").css("margin-right", originalMarginRight);
+            currentState = 'original';
+        }
+    });
+  
+    $(".c1").click(function() {
+        if (currentState !== 'calt') {
+            $(".skill1").css({"width": CaltWidth, "height": CaltHeight});
+            $(".ms1b h1").text("저의 미래를 기대하세요!");
+            $(".ms1b h1").css("font-size", CaltFontSize);
+            $(".ms1b h1").css("color", CaltFontColor);
+            $(".ms1t ul").css("margin-left", CMarginLeft);
+            $(".ms1t ul li").css("margin-right", CMarginRight);
+            currentState = 'calt';
+        } else {
+            $(".skill1").css({"width": originalWidth, "height": originalHeight});
+            $(".ms1b h1").text("ES5");
+            $(".ms1b h1").css("font-size", originalFontSize);
+            $(".ms1b h1").css("color", originalFontColor);
+            $(".ms1t ul").css("margin-left", originalMarginLeft);
+            $(".ms1t ul li").css("margin-right", originalMarginRight);
+            currentState = 'original';
+        }
+    });
+
+    //javascript
+    $(".s2").click(function() {
+        if (currentState !== 'salt') {
+            $(".skill2").css({"width": SaltWidth, "height": SaltHeight});
+            $(".ms2b h1").text("가장 기본이 되는 vanilla javascript를 사용하기 위해 꾸준한 노력을 하고 있습니다.");
+            $(".ms2b h1").css({"font-size": SaltFontSize, "color": originalFontColor});
+            $(".ms2t ul").css("margin-left", SMarginLeft);
+            $(".ms2t ul li").css("margin-right", SMarginRight);
+            currentState = 'salt';
+        } else {
+            $(".skill2").css({"width": originalWidth, "height": originalHeight});
+            $(".ms2b h1").text("Javascript");
+            $(".ms2b h1").css("font-size", originalFontSize);
+            $(".ms2b h1").css("color", originalFontColor);
+            $(".ms2t ul").css("margin-left", originalMarginLeft);
+            $(".ms2t ul li").css("margin-right", originalMarginRight);
+            currentState = 'original';
+        }
+    });
+  
+    $(".b2").click(function() {
+        if (currentState !== 'balt') {
+            $(".skill2").css({"width": BaltWidth, "height": BaltHeight});
+            $(".ms2b h1").text("vanilla javscript에서 jQuery로도 자유자재로 사용하며 효율성을 높일 것 입니다.");
+            $(".ms2b h1").css({"font-size": BaltFontSize, "color": originalFontColor});
+            $(".ms2t ul").css("margin-left", BMarginLeft);
+            $(".ms2t ul li").css("margin-right", BMarginRight);
+            currentState = 'balt';
+        } else {
+            $(".skill2").css({"width": originalWidth, "height": originalHeight});
+            $(".ms2b h1").text("Javascript");
+            $(".ms2b h1").css("font-size", originalFontSize);
+            $(".ms2b h1").css("color", originalFontColor);
+            $(".ms2t ul").css("margin-left", originalMarginLeft);
+            $(".ms2t ul li").css("margin-right", originalMarginRight);
+            currentState = 'original';
+        }
+    });
+  
+    $(".c2").click(function() {
+        if (currentState !== 'calt') {
+            $(".skill2").css({"width": CaltWidth, "height": CaltHeight});
+            $(".ms2b h1").text("저의 열정은 꺼지지 않아요!");
+            $(".ms2b h1").css("font-size", CaltFontSize);
+            $(".ms2b h1").css("color", CaltFontColor);
+            $(".ms2t ul").css("margin-left", CMarginLeft);
+            $(".ms2t ul li").css("margin-right", CMarginRight);
+            currentState = 'calt';
+        } else {
+            $(".skill2").css({"width": originalWidth, "height": originalHeight});
+            $(".ms2b h1").text("Javascript");
+            $(".ms2b h1").css("font-size", originalFontSize);
+            $(".ms2b h1").css("color", originalFontColor);
+            $(".ms2t ul").css("margin-left", originalMarginLeft);
+            $(".ms2t ul li").css("margin-right", originalMarginRight);
+            currentState = 'original';
+        }
+    });
+
+
+//html5
+    $(".s3").click(function() {
         if (currentState !== 'salt') {
             $(".skill3").css({"width": SaltWidth, "height": SaltHeight});
-            $(".ms3b h1").text("저는 기본이 되는 마크업 조차도 구현할 줄 몰랐어요");
+            $(".ms3b h1").text("웹 표준에 맞춰 시멘틱 태그를 사용해 마크업을 자유롭게 할 수 있습니다.");
             $(".ms3b h1").css({"font-size": SaltFontSize, "color": originalFontColor});
             $(".ms3t ul").css("margin-left", SMarginLeft);
             $(".ms3t ul li").css("margin-right", SMarginRight);
@@ -48,10 +168,10 @@ $(document).ready(function() {
         }
     });
   
-    $(".b").click(function() {
+    $(".b3").click(function() {
         if (currentState !== 'balt') {
             $(".skill3").css({"width": BaltWidth, "height": BaltHeight});
-            $(".ms3b h1").text("이젠 웹 기술의 폭넓은 범위를 탐험하며, 새로운 기술들을 자신 있게 습득해나가고 있어요");
+            $(".ms3b h1").text("반응형까지 기본구조를 지키며 자유롭게 구현하며, 화면을 줄이거나 늘려도 흐트러짐이 없도록 마크업 할 예정입니다.");
             $(".ms3b h1").css({"font-size": BaltFontSize, "color": originalFontColor});
             $(".ms3t ul").css("margin-left", BMarginLeft);
             $(".ms3t ul li").css("margin-right", BMarginRight);
@@ -67,10 +187,10 @@ $(document).ready(function() {
         }
     });
   
-    $(".c").click(function() {
+    $(".c3").click(function() {
         if (currentState !== 'calt') {
             $(".skill3").css({"width": CaltWidth, "height": CaltHeight});
-            $(".ms3b h1").text("저의 열정은 꺼지지 않아요!");
+            $(".ms3b h1").text("저는 분명 해낼겁니다!");
             $(".ms3b h1").css("font-size", CaltFontSize);
             $(".ms3b h1").css("color", CaltFontColor);
             $(".ms3t ul").css("margin-left", CMarginLeft);
@@ -86,6 +206,248 @@ $(document).ready(function() {
             currentState = 'original';
         }
     });
+
+
+
+//css
+    $(".s4").click(function() {
+        if (currentState !== 'salt') {
+            $(".skill4").css({"width": SaltWidth, "height": SaltHeight});
+            $(".ms4b h1").text("css의 기본 순서를 지키려고 노력하며, 남이 봐도 알아보기 쉽게 적으려 합니다.");
+            $(".ms4b h1").css({"font-size": SaltFontSize, "color": originalFontColor});
+            $(".ms4t ul").css("margin-left", SMarginLeft);
+            $(".ms4t ul li").css("margin-right", SMarginRight);
+            currentState = 'salt';
+        } else {
+            $(".skill4").css({"width": originalWidth, "height": originalHeight});
+            $(".ms4b h1").text("CSS");
+            $(".ms4b h1").css("font-size", originalFontSize);
+            $(".ms4b h1").css("color", originalFontColor);
+            $(".ms4t ul").css("margin-left", originalMarginLeft);
+            $(".ms4t ul li").css("margin-right", originalMarginRight);
+            currentState = 'original';
+        }
+    });
+  
+    $(".b4").click(function() {
+        if (currentState !== 'balt') {
+            $(".skill4").css({"width": BaltWidth, "height": BaltHeight});
+            $(".ms4b h1").text("HTML로 마크업을 하고 동적일 수 있는 부분에 대해서는 모든 css 태그를 알고 사용할 수 있도록 할 예정입니다.");
+            $(".ms4b h1").css({"font-size": BaltFontSize, "color": originalFontColor});
+            $(".ms4t ul").css("margin-left", BMarginLeft);
+            $(".ms4t ul li").css("margin-right", BMarginRight);
+            currentState = 'balt';
+        } else {
+            $(".skill4").css({"width": originalWidth, "height": originalHeight});
+            $(".ms4b h1").text("CSS");
+            $(".ms4b h1").css("font-size", originalFontSize);
+            $(".ms4b h1").css("color", originalFontColor);
+            $(".ms4t ul").css("margin-left", originalMarginLeft);
+            $(".ms4t ul li").css("margin-right", originalMarginRight);
+            currentState = 'original';
+        }
+    });
+  
+    $(".c4").click(function() {
+        if (currentState !== 'calt') {
+            $(".skill4").css({"width": CaltWidth, "height": CaltHeight});
+            $(".ms4b h1").text("저의 열정은 꺼지지 않아요!");
+            $(".ms4b h1").css("font-size", CaltFontSize);
+            $(".ms4b h1").css("color", CaltFontColor);
+            $(".ms4t ul").css("margin-left", CMarginLeft);
+            $(".ms4t ul li").css("margin-right", CMarginRight);
+            currentState = 'calt';
+        } else {
+            $(".skill4").css({"width": originalWidth, "height": originalHeight});
+            $(".ms4b h1").text("CSS");
+            $(".ms4b h1").css("font-size", originalFontSize);
+            $(".ms4b h1").css("color", originalFontColor);
+            $(".ms4t ul").css("margin-left", originalMarginLeft);
+            $(".ms4t ul li").css("margin-right", originalMarginRight);
+            currentState = 'original';
+        }
+    });
+
+//react.js
+    $(".s5").click(function() {
+        if (currentState !== 'salt') {
+            $(".skill5").css({"width": SaltWidth, "height": SaltHeight});
+            $(".ms5b h1").text("HTML, CSS,javascript에서 더 나아가 사용자와 상호작용 할 수 있게 동적이고 쉬운 UI를 만들고자 공부하고 있습니다.");
+            $(".ms5b h1").css({"font-size": SaltFontSize, "color": originalFontColor});
+            $(".ms5t ul").css("margin-left", SMarginLeft);
+            $(".ms5t ul li").css("margin-right", SMarginRight);
+            currentState = 'salt';
+        } else {
+            $(".skill5").css({"width": originalWidth, "height": originalHeight});
+            $(".ms5b h1").text("React.js");
+            $(".ms5b h1").css("font-size", originalFontSize);
+            $(".ms5b h1").css("color", originalFontColor);
+            $(".ms5t ul").css("margin-left", originalMarginLeft);
+            $(".ms5t ul li").css("margin-right", originalMarginRight);
+            currentState = 'original';
+        }
+    });
+  
+    $(".b5").click(function() {
+        if (currentState !== 'balt') {
+            $(".skill5").css({"width": BaltWidth, "height": BaltHeight});
+            $(".ms5b h1").text("사용자가 보다 더 쉽게 웹 페이지를 이용할 수 있도록 react.js를 사용할 예정입니다.");
+            $(".ms5b h1").css({"font-size": BaltFontSize, "color": originalFontColor});
+            $(".ms5t ul").css("margin-left", BMarginLeft);
+            $(".ms5t ul li").css("margin-right", BMarginRight);
+            currentState = 'balt';
+        } else {
+            $(".skill5").css({"width": originalWidth, "height": originalHeight});
+            $(".ms5b h1").text("React.js");
+            $(".ms5b h1").css("font-size", originalFontSize);
+            $(".ms5b h1").css("color", originalFontColor);
+            $(".ms5t ul").css("margin-left", originalMarginLeft);
+            $(".ms5t ul li").css("margin-right", originalMarginRight);
+            currentState = 'original';
+        }
+    });
+  
+    $(".c5").click(function() {
+        if (currentState !== 'calt') {
+            $(".skill5").css({"width": CaltWidth, "height": CaltHeight});
+            $(".ms5b h1").text("저를 뽑아주세요!");
+            $(".ms5b h1").css("font-size", CaltFontSize);
+            $(".ms5b h1").css("color", CaltFontColor);
+            $(".ms5t ul").css("margin-left", CMarginLeft);
+            $(".ms5t ul li").css("margin-right", CMarginRight);
+            currentState = 'calt';
+        } else {
+            $(".skill5").css({"width": originalWidth, "height": originalHeight});
+            $(".ms5b h1").text("React.js");
+            $(".ms5b h1").css("font-size", originalFontSize);
+            $(".ms5b h1").css("color", originalFontColor);
+            $(".ms5t ul").css("margin-left", originalMarginLeft);
+            $(".ms5t ul li").css("margin-right", originalMarginRight);
+            currentState = 'original';
+        }
+    });
+
+//jquery
+    $(".s6").click(function() {
+        if (currentState !== 'salt') {
+            $(".skill6").css({"width": SaltWidth, "height": SaltHeight});
+            $(".ms6b h1").text("js를 보다 더 쉽고 간소화하기 위해 사용하지만 vanilla js를 기반으로 공부하고 있습니다.");
+            $(".ms6b h1").css({"font-size": SaltFontSize, "color": originalFontColor});
+            $(".ms6t ul").css("margin-left", SMarginLeft);
+            $(".ms6t ul li").css("margin-right", SMarginRight);
+            currentState = 'salt';
+        } else {
+            $(".skill6").css({"width": originalWidth, "height": originalHeight});
+            $(".ms6b h1").text("jQuery");
+            $(".ms6b h1").css("font-size", originalFontSize);
+            $(".ms6b h1").css("color", originalFontColor);
+            $(".ms6t ul").css("margin-left", originalMarginLeft);
+            $(".ms6t ul li").css("margin-right", originalMarginRight);
+            currentState = 'original';
+        }
+    });
+  
+    $(".b6").click(function() {
+        if (currentState !== 'balt') {
+            $(".skill6").css({"width": BaltWidth, "height": BaltHeight});
+            $(".ms6b h1").text("javascript와 jQuery의 문법,언어의 차이를 제대로 알며 사용 할 예정입니다.");
+            $(".ms6b h1").css({"font-size": BaltFontSize, "color": originalFontColor});
+            $(".ms6t ul").css("margin-left", BMarginLeft);
+            $(".ms6t ul li").css("margin-right", BMarginRight);
+            currentState = 'balt';
+        } else {
+            $(".skill6").css({"width": originalWidth, "height": originalHeight});
+            $(".ms6b h1").text("jQuery");
+            $(".ms6b h1").css("font-size", originalFontSize);
+            $(".ms6b h1").css("color", originalFontColor);
+            $(".ms6t ul").css("margin-left", originalMarginLeft);
+            $(".ms6t ul li").css("margin-right", originalMarginRight);
+            currentState = 'original';
+        }
+    });
+  
+    $(".c6").click(function() {
+        if (currentState !== 'calt') {
+            $(".skill6").css({"width": CaltWidth, "height": CaltHeight});
+            $(".ms6b h1").text("저는 최선을 다합니다!");
+            $(".ms6b h1").css("font-size", CaltFontSize);
+            $(".ms6b h1").css("color", CaltFontColor);
+            $(".ms6t ul").css("margin-left", CMarginLeft);
+            $(".ms6t ul li").css("margin-right", CMarginRight);
+            currentState = 'calt';
+        } else {
+            $(".skill6").css({"width": originalWidth, "height": originalHeight});
+            $(".ms6b h1").text("jQuery");
+            $(".ms6b h1").css("font-size", originalFontSize);
+            $(".ms6b h1").css("color", originalFontColor);
+            $(".ms6t ul").css("margin-left", originalMarginLeft);
+            $(".ms6t ul li").css("margin-right", originalMarginRight);
+            currentState = 'original';
+        }
+    });
+
+//sass
+    $(".s7").click(function() {
+        if (currentState !== 'salt') {
+            $(".skill7").css({"width": SaltWidth, "height": SaltHeight});
+            $(".ms7b h1").text("CSS의 단점을 보완할 수 있도록 공부하며, 코드작성 시간을 줄이려고 노력합니다.");
+            $(".ms7b h1").css({"font-size": SaltFontSize, "color": originalFontColor});
+            $(".ms7t ul").css("margin-left", SMarginLeft);
+            $(".ms7t ul li").css("margin-right", SMarginRight);
+            currentState = 'salt';
+        } else {
+            $(".skill7").css({"width": originalWidth, "height": originalHeight});
+            $(".ms7b h1").text("SASS");
+            $(".ms7b h1").css("font-size", originalFontSize);
+            $(".ms7b h1").css("color", originalFontColor);
+            $(".ms7t ul").css("margin-left", originalMarginLeft);
+            $(".ms7t ul li").css("margin-right", originalMarginRight);
+            currentState = 'original';
+        }
+    });
+  
+    $(".b7").click(function() {
+        if (currentState !== 'balt') {
+            $(".skill7").css({"width": BaltWidth, "height": BaltHeight});
+            $(".ms7b h1").text("CSS의 확장 언어지만 더 다양하고 동적인 코드를 위해 배워 기능들을 사용할겁니다.");
+            $(".ms7b h1").css({"font-size": BaltFontSize, "color": originalFontColor});
+            $(".ms7t ul").css("margin-left", BMarginLeft);
+            $(".ms7t ul li").css("margin-right", BMarginRight);
+            currentState = 'balt';
+        } else {
+            $(".skill7").css({"width": originalWidth, "height": originalHeight});
+            $(".ms7b h1").text("SASS");
+            $(".ms7b h1").css("font-size", originalFontSize);
+            $(".ms7b h1").css("color", originalFontColor);
+            $(".ms7t ul").css("margin-left", originalMarginLeft);
+            $(".ms7t ul li").css("margin-right", originalMarginRight);
+            currentState = 'original';
+        }
+    });
+  
+    $(".c7").click(function() {
+        if (currentState !== 'calt') {
+            $(".skill7").css({"width": CaltWidth, "height": CaltHeight});
+            $(".ms7b h1").text("저와 함께해요!");
+            $(".ms7b h1").css("font-size", CaltFontSize);
+            $(".ms7b h1").css("color", CaltFontColor);
+            $(".ms7t ul").css("margin-left", CMarginLeft);
+            $(".ms7t ul li").css("margin-right", CMarginRight);
+            currentState = 'calt';
+        } else {
+            $(".skill7").css({"width": originalWidth, "height": originalHeight});
+            $(".ms7b h1").text("SASS");
+            $(".ms7b h1").css("font-size", originalFontSize);
+            $(".ms7b h1").css("color", originalFontColor);
+            $(".ms7t ul").css("margin-left", originalMarginLeft);
+            $(".ms7t ul li").css("margin-right", originalMarginRight);
+            currentState = 'original';
+        }
+    });
+
+
+    
+
   });
   
   
